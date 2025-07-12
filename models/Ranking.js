38aa52881c99c6f09a21a32a142eee9679e3a256
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-07-12 15:59:35 +0800

    pooosh

--- models/Ranking.js
+++ models/Ranking.js
@@ -56,14 +56,65 @@ const RankingHistorySchema = new mongoose.Schema(
     { timestamps: true }
 );
 
+const RankRewardSchema = new mongoose.Schema(
+    {
+        rank: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "RankTier",
+            required: true
+        },
+        rewards: [
+            {
+                rewardtype: {
+                    type: String,
+                    enum: ['badge', 'title', 'weapon', 'outfit', 'exp', 'coins', 'crystal'],
+                    required: true
+                },
+                amount: {
+                    type: Number,
+                },
+                reward: {
+                    type: Object,
+                }
+            }
+        ]
+        
+    },
+    {
+        timestamps: true
+    }
+)
+
 const RankingHistory = mongoose.model("RankingHistory", RankingHistorySchema);
 const Rankings = mongoose.model("Rankings", RankingSchema);
+const RankReward = mongoose.model("RankReward", RankRewardSchema);
+
 module.exports = {
     Rankings,
-    RankingHistory
+    RankingHistory,
+    RankReward
 };
 
-
+// {
+//     "rank": "64f8b2c3d1e4a5b6c7d8e9f0",
+//     "rewards": [
+//         {
+//             "rewardtype": "skin",
+//             "amount": 1,
+//             "reward": {
+//                 "id": "male_skin_id",
+//                 "fid": "female_skin_id",
+//             }
+//         },
+//         {
+//             "rewardtype": "crystal",
+//             "amount": 500,
+//             "reward": {
+//                 "amount": 500
+//             }
+//         }
+//     ]
+// }
 
 
 // const mongoose = require("mongoose");
